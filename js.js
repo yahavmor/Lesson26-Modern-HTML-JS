c2c9a6b069c001cb47cc0880e2d1d37dd3139923
yahav mor <yahavmor77@gmail.com>
2025-08-06 11:27:52 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,21 @@
+'use strict';
+  
+const defaultPrefs = {
+        bgc: 'white',
+        txt: 'black'
+    }; 
+function setPrefs(){
+    const userPrefs = JSON.parse(localStorage.getItem('userPrefs'));
+    if (!userPrefs.bgc || !userPrefs.txt) {
+        document.body.style.backgroundColor = defaultPrefs.bgc;
+        document.body.style.color = defaultPrefs.txt;
+    }else {
+        document.body.style.backgroundColor = userPrefs.bgc;
+        document.body.style.color = userPrefs.txt;
+    }
+}
+
+function onResetPrefs(){
+    localStorage.setItem('userPrefs', JSON.stringify(defaultPrefs));
+    setPrefs();
+}
